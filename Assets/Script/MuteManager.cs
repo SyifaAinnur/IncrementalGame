5f270e378377b116a453df34b535ddd45619e416
Syifa Ainnur <arwahitem01@gmail.com>
2021-09-11 12:07:26 +0700

    update commit

---
+++ Assets/Script/MuteManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteManager : MonoBehaviour
+{
+    private bool isMuted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMuted = false;
+    }
+
+    public void MutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+    }
+    // Update is called once per frame
+
+}
